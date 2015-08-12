72c0b25fddbfba36c5bb03dcd183b56103b1f5f5
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-08-12 09:25:59 -0700

    fixed viewport on schedule

--- public/javascripts/spotify.js
+++ public/javascripts/spotify.js
@@ -29,7 +29,7 @@ function loadArtistData(index,callback){
 			var img = new Image();
 			img.src=data.artists.items[0].images[1].url;
 			unseen[index].artistImgPath = data.artists.items[0].images[1].url;
-			console.log("ARtist Data : ",data.artists.items[0]);
+			console.log("Artist Data : ",data.artists.items[0]);
 			var artistId = data.artists.items[0].id;
 			$.ajax({
 				url: "http://developer.echonest.com/api/v4/artist/biographies?api_key=BKTYVQYVRUPS203VV&id=spotify:artist:"+artistId+"&format=json&results=1&start=0&license=cc-by-sa",
