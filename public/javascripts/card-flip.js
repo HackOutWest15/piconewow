9d8548835e868dca797b148e165008585271ccc9
Sebastian Sandberg <seb.sandberg@gmail.com>
2015-08-12 10:14:13 -0700

    added tutorial on swipe

--- public/javascripts/card-flip.js
+++ public/javascripts/card-flip.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+	$('.Wizard-box').fadeIn();
+	$('.Wizard-box').on(clickOrTouch,function(evt){
+      $(this).hide();
+    });
 	loadArtistData(0, function(){
 		popShow();
+
 /*		$(".flip-container").on(clickOrTouch,function(e) {
 			$(".flipper").toggleClass("is-flipped");
 		});*/
